760563569e06faa1d5ff27d5e107ee87e0f894a3
Tomás Solar Castro <tsolar@gmail.com>
2017-02-12 21:47:07 -0300

    added user menu
    * Added menu for signed in/out user
    
    this closes #2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
+
+  def username
+    email
+  end
 end
